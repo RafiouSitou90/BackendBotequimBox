8b10de99d70037d119c476a94ce218a7c36acf24
unknown <MatheusRCapanema>
2023-04-04 14:48:41 -0300

    Initial commit

---
+++ src/main/java/com/example/backendbotequimbox/BackendBotequimBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendbotequimbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBotequimBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBotequimBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendbotequimbox/BackendBotequimBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendbotequimbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBotequimBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
